744d2becd620fa68db65beb2ee4f3b07c88c418b
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-04-28 14:56:17 -0300

    Update ex-01.c

--- Aula 03/ex-01.c
+++ Aula 03/ex-01.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(){
+    int num1, num2, num3, soma;
+    printf("Digite o primeiro valor: ");
+    scanf("%d",&num1);
+    printf("Digite o segundo valor: ");
+    scanf("%d",&num2);
+    printf("Digite o terceiro valor: ");
+    scanf("%d",&num3);
+
+    soma = num1 + num2 + num3;
+
+    printf("Soma: %d\n", soma);
+
+    return 0;
+}
