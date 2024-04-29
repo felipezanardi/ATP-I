9433eb7095b360a49f14f3511c61d8b3b4861b3d
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-04-29 07:21:42 -0300

    Update ex-01.c

--- Aula 04/ex-01.c
+++ Aula 04/ex-01.c
@@ -1,1 +1,13 @@
+/*Faça um programa que leia um número inteiro e o imprima.*/
 
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d",&num);
+
+    printf("Numero digitado: %d\n", num);
+
+    return 0;
+}
