d3d084c45b34376642bf648fb7855401810c548d
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-04-29 14:10:41 -0300

    Create ex-02.c

---
+++ Aula 04/ex-02.c
@@ -0,0 +1,13 @@
+/*Faça um programa que leia um número real e o imprima.*/
+
+#include <stdio.h>
+
+int main(){
+    float num;
+    printf("Digite um numero: ");
+    scanf("%f",&num);
+
+    printf("Numero digitado: %f\n", num);
+
+    return 0;
+}
