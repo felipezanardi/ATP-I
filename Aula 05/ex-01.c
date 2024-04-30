23b2a7f056a366766e9fd69234ff16daa9e3e028
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-04-30 19:35:15 -0300

    Update ex-01.c

--- Aula 05/ex-01.c
+++ Aula 05/ex-01.c
@@ -1,1 +1,19 @@
+/*Faça um programa para calcular a quantidade de cupons que um cliente recebe ao realizar
+uma compra em um supermercado. A partir do valor gasto na compra o programa exibe a
+quantidade de cupons, considerando que o cliente recebe um cupom a cada R$ 20,00 em
+compras.*/
 
+#include <stdio.h>
+
+int main(){
+    float gasto;
+    int cupom;
+    printf("Valor gasto: R$");
+    scanf("%f",&gasto);
+
+    cupom = gasto / 20;
+
+    printf("Cliente recebeu %d cupom/cupons.\n", cupom);
+
+    return 0;
+}
