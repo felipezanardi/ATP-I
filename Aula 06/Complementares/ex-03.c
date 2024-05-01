233e1b1e2402047bfb383e1bd09549fe168db207
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-01 16:38:08 -0300

    Create ex-03.c

---
+++ Aula 06/Complementares/ex-03.c
@@ -0,0 +1,20 @@
+/*Faça um programa para ler o salário de um trabalhador e o valor da prestação de um
+empréstimo. Se a prestação for maior que 20% do salário, o programa deverá imprimir
+“Empréstimo não concedido”; caso contrário imprimirá “Empréstimo concedido”.*/
+
+#include <stdio.h>
+int main(){
+    float salario, prestacao;
+    printf("Digite o salario: ");
+    scanf("%f",&salario);
+    printf("Digite o valor da prestacao: ");
+    scanf("%f",&prestacao);
+
+    if(prestacao > salario * 0.2){
+        printf("Emprestimo nao concedido");
+    }else{
+        printf("Emprestimo concedido");
+    }
+
+    return 0;
+}
