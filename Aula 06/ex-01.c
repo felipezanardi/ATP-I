522e5f7e60670232a7d18abddc7da389963958f5
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-01 01:15:31 -0300

    Update ex-01.c

--- Aula 06/ex-01.c
+++ Aula 06/ex-01.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int main(){
     int idade;
-    printf("Digite sua idade: ");
+    printf("Digite a idade: ");
     scanf("%d",&idade);
     if(idade >= 18){
         printf("Maior de idade.\n");
