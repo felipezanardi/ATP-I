847c85145dbbbc09f78f12c06e4d2522eea4b8ea
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-01 01:12:58 -0300

    Update ex-01.c

--- Aula 06/ex-01.c
+++ Aula 06/ex-01.c
@@ -1,1 +1,14 @@
+/*Fazer um programa para ler a idade de uma pessoa e verificar se ela é menor ou maior.*/
 
+#include <stdio.h>
+int main(){
+    int idade;
+    printf("Digite sua idade: ");
+    scanf("%d",&idade);
+    if(idade >= 18){
+        printf("Maior de idade.\n");
+    }else{
+        printf("Menor de idade.\n");
+    }
+    return 0;
+}
