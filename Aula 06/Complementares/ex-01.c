202614927e68d627eb0df38e9811ab23d1e8db17
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-01 16:05:46 -0300

    Update ex-01.c

--- Aula 06/Complementares/ex-01.c
+++ Aula 06/Complementares/ex-01.c
@@ -1,1 +1,16 @@
+/*Faça um programa para ler um número inteiro e verificar se este número é par ou ímpar.*/
 
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Digite um numero inteiro: ");
+    scanf("%d",&num);
+
+    if(num % 2 == 0){
+        printf("Numero par");
+    }else{
+        printf("Numero impar");
+    }
+
+    return 0;
+}
