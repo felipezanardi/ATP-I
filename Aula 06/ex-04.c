cbe739e5c7deaf3d1b398b582c509065c0f18df8
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-01 14:38:29 -0300

    Create ex-04.c

---
+++ Aula 06/ex-04.c
@@ -0,0 +1,18 @@
+/*4) Fazer um programa para ler dois números e verificar se são iguais ou diferentes.*/
+
+#include <stdio.h>
+int main(){
+    float num1, num2;
+    printf("Primeiro numero: ");
+    scanf("%f",&num1);
+    printf("Segundo numero: ");
+    scanf("%f",&num2);
+
+    if(num1 == num2){
+        printf("Os numeros sao iguais");
+    }else{
+        printf("Os numeros sao diferentes");
+    }
+
+    return 0;
+}
