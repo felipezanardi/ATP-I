2d0a598f076e443d7c25ca6c51fd82036517f1f7
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-04 13:06:37 -0300

    Update ex-03.c

--- Aula 09/ex-03.c
+++ Aula 09/ex-03.c
@@ -3,14 +3,10 @@
 #include <stdio.h>
 int main(){
     int valor, i=0, maior;
-    printf("Digite o 1o valor: ");
-    scanf("%d",&maior);
-    i++;
-
     while(i < 5){
         printf("Digite o %do valor: ", i+1);
         scanf("%d",&valor);
-        if(valor > maior){
+        if(valor > maior || i == 0){
             maior = valor;
         }
         i++;
