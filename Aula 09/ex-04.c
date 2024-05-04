be1572b601dc6631c6dac70304f0a386b80c9c3d
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-04 13:14:32 -0300

    Create ex-04.c

---
+++ Aula 09/ex-04.c
@@ -0,0 +1,17 @@
+/*Ler 5 valores do usuário e apresentar o menor valor.*/
+
+#include <stdio.h>
+int main(){
+    int valor, i=0, menor;
+    while(i < 5){
+        printf("Digite o %do valor: ", i+1);
+        scanf("%d",&valor);
+        if(valor < menor || i == 0){
+            menor = valor;
+        }
+        i++;
+    }
+    printf("\nMenor valor: %d\n", menor);
+
+    return 0;
+}
