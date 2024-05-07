0b7b296e323b162b8ec14906aa6322622826451f
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-07 08:22:45 -0300

    Create ex-02.c

---
+++ Aula 10/ex-02.c
@@ -0,0 +1,21 @@
+/*(Utilize o comando 'do while') Ler quantos valores serão fornecidos pelo usuário. Em seguida, fazer a
+leitura desses valores. Por fim, apresentar a soma e a média.*/
+
+#include <stdio.h>
+
+int main(){
+    int quant, i=0;
+    float valor, soma, maior, menor;
+    printf("Quantidade de valores: ");
+    scanf("%d",&quant);
+    do{
+        printf("Informe o %do valor: ", i+1);
+        scanf("%f",&valor);
+        soma = soma + valor;
+        i++;
+    }while(i < quant);
+
+    printf("\nSoma: %f\n"
+           "Media: %f\n", soma, soma/quant);
+    return 0;
+}
