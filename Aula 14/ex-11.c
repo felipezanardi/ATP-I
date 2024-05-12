323df8d78035ea4f11e0d162f5c4070a589a2cf3
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-12 14:19:39 -0300

    Create ex-11.c

---
+++ Aula 14/ex-11.c
@@ -0,0 +1,18 @@
+/*
+Faça um programa para ler uma string e transferir o conteúdo para uma outra
+varíavel string (char []).
+*/
+
+#include <stdio.h>
+int main(){
+    char str1[100], str2[100];
+    int check=0;
+    printf("Digite algo: ");
+    gets(str1);
+
+    for(int i=0; i<100; i++){
+        str2[i] = str1[i];
+    }
+    printf("Outra string: %s", str2);
+    return 0;
+}
