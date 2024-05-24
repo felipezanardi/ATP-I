2fb4f4d96fd53f5c805cb8e912504180984a5ffe
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-05-24 19:38:13 -0300

    Create ex-07.c

---
+++ Aula 16/ex-07.c
@@ -0,0 +1,24 @@
+/*
+Faça uma função denominada maiusculo para receber uma string e converter
+seus caracteres para maiúsculo.
+*/
+
+#include <stdio.h>
+#include <string.h>
+int maiusculo(char str[100]){
+    for(int i=0; i<strlen(str); i++){
+        if(str[i] >= 'a' && str[i] <= 'z'){
+            str[i] -= 32;
+        }
+    }
+    return str;
+}
+
+int main(){
+    char str[100];
+    printf("Digite um string: ");
+    gets(str);
+
+    printf("String modificada: %s\n", maiusculo(str));
+    return 0;
+}
