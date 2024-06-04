9cb45aeb0325c8601c2a8b9227b0ac1b783ff3f9
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-06-04 19:56:05 -0300

    Create ex-09.c

---
+++ Aula 16/ex-09.c
@@ -0,0 +1,34 @@
+/*
+Faça uma função denominada remover_espacos para receber uma string e
+remover espaços desnecessários, ou seja, considerar somente um espaço entre as
+palavras da string.
+*/
+
+#include <stdio.h>
+#include <string.h>
+char remover_espacos(char a[]){
+    for(int i=0; a[0] == ' '; i++){
+        for(int m=0; m<strlen(a); m++){
+            a[m] = a[m+1];
+        }
+    }
+    for(int i=0; i<strlen(a); i++){
+        if(a[i] == ' ' && a[i+1] == ' '){
+            for(int m=i; m<strlen(a); m++){
+                a[m] = a[m+1];
+            }
+            i--;
+        }
+    }
+}
+
+int main(){
+    char str[200];
+    printf("Digite uma frase: ");
+    gets(str);
+
+    printf("Frase alterada: ");
+    remover_espacos(str);
+    printf("%s\n", str);
+    return 0;
+}
