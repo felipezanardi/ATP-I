efa4d9890032e5459b7f415c4448046277200bbb
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-06-06 10:14:46 -0300

    Update ex-01.c

--- Aula 17/ex-01.c
+++ Aula 17/ex-01.c
@@ -1,1 +1,20 @@
+/*
+Faça uma função para calcular o fatorial de um número e retornar por parâmetro
+o valor do fatorial. Por exemplo: void fatorial (int n, int *fat);
+*/
 
+#include <stdio.h>
+void fatorial(int n, int *fat){
+    for(int i=2; i<=n; i++){
+        *fat *= i;
+    }
+}
+
+int main (){
+    int n, fat=1;
+    printf("Digite um numero: ");
+    scanf("%d",&n);
+    fatorial(n, &fat);
+    printf("Fatorial: %d\n", fat);
+    return 0;
+}
