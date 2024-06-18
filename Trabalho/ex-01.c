ceba8447f4e581ba8f976a931e6730a56e7b091c
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-06-18 10:28:10 -0300

    Update ex-01.c

--- Trabalho/ex-01.c
+++ Trabalho/ex-01.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+void codificar(int k, char a[]){
+    for(int i=0; i<strlen(a); i++){
+        a[i] -= k;
+    }
+}
 
+void descodificar(int k, char a[]){
+    for(int i=0; i<strlen(a); i++){
+        a[i] += k;
+    }
+}
+int main(){
+    int k;
+    char frase[100];
+
+    printf("Digite uma frase: ");
+    gets(frase);
+    printf("Valor k: ");
+    scanf("%d",&k);
+    fflush(stdin);
+    codificar(k, frase);
+    printf("Palavra codificada: %s\n", frase);
+
+    printf("Digite uma frase codificada: ");
+    gets(frase);
+    printf("Valor k: ");
+    scanf("%d",&k);
+    descodificar(k, frase);
+    printf("Palavra descodificada: %s\n", frase);
+    return 0;
+}
